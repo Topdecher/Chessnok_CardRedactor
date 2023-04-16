9eb1c2c4cb28c8dba7326e5b0d6e0d6975804448
Topdecher <d.kovalenko.nt@gmail.com>
2023-04-16 21:58:09 +0300

    advanced parsing, cleanup

--- image_redactor/content_box.py
+++ image_redactor/content_box.py
@@ -1,24 +1,61 @@
 from PIL import ImageDraw, ImageFont, Image
+
+from image_redactor.card_drawer import CardDrawer
 from image_redactor.constants import *
 
 
-class Box:
-    def __init__(self, drawer, width=0, height=0):
-        self.drawer = drawer
-        self.width = width
-        self.height = height
+class BitmapBox:
+    def __init__(self, bitmap, bitmap_path, default_position=(0, 0), can_centralize=True):
+        self.bitmap = bitmap
+        self.bitmap_path = bitmap_path
+        self.position = default_position
+        self.default_position = default_position
+        self.can_centralize = can_centralize
+        self.width = 0
+        self.height = 0
 
-    def parse_content(self): pass
+    def set_bitmap(self, bitmap):
+        self.bitmap = bitmap
 
-    def update_content(self, content): pass
+    def set_bitmap_path(self, bitmap_path):
+        self.bitmap_path = bitmap_path
+
+    def set_position(self, pos_x, pos_y):
+        self.position = (pos_x, pos_y)
+
+    def move(self, vector):
+        self.position = (self.position[0] + vector[0], self.position[1] + vector[1])
+
+    def discard_position(self):
+        self.position = self.default_position
+
+    def get_position(self):
+        return self.position
+
+    def get_bitmap_width(self):
+        if self.bitmap is None:
+            return 0
+        return self.bitmap.Size[0]
+
+    def get_bitmap_height(self):
+        if self.bitmap is None:
+            return 0
+        return self.bitmap.Size[1]
+
+    def get_bitmap_size(self):
+        if self.bitmap is None:
+            return 0, 0
+        return self.bitmap.GetSize()
 
 
-class TextBox(Box):
-    def __init__(self, drawer, content='', font_path=DEFAULT_FONT, default_font_size=DEFAULT_TEXT_FONT_SIZE,
-                 min_font_size=MIN_FONT_SIZE, size_step=SIZE_STEP, spacing=SPACING,
-                 max_width=MAX_TEXT_WIDTH, max_height=MAX_TEXT_HEIGHT):
-        super().__init__(drawer)
+class TextBox(BitmapBox):
+    def __init__(self, bitmap, bitmap_path, content='', font_path=DEFAULT_FONT,
+                 default_font_size=DEFAULT_TEXT_FONT_SIZE, min_font_size=MIN_FONT_SIZE,
+                 size_step=SIZE_STEP, spacing=SPACING,
+                 max_width=MAX_TEXT_WIDTH, max_height=MAX_TEXT_HEIGHT, case=LOWER_CASE, **kwargs):
+        super().__init__(bitmap, bitmap_path, **kwargs)
         self.content = content
+        self.font = None
         self.font_path = font_path
         self.default_font_size = default_font_size
         self.font_size = default_font_size
@@ -27,6 +64,7 @@ class TextBox(Box):
         self.max_width = max_width
         self.max_height = max_height
         self.spacing = spacing
+        self.case = case
         self.text = self.parse_content()
 
     def discard_font_size(self):
@@ -54,14 +92,14 @@ class TextBox(Box):
         text = ''
         self.discard_font_size()
         while self.font_size >= self.min_font_size:
-            font = ImageFont.truetype(font=self.font_path, size=self.font_size, encoding='UTF-8')
+            self.font = ImageFont.truetype(font=self.font_path, size=self.font_size, encoding='UTF-8')
             text = ''
-            words = self.smart_split(self.content)
-            print(words)
+            words = self.smart_split(self.content.lower() if self.case == LOWER_CASE else self.content.upper())
             word_counter = 0
             while word_counter < len(words):
-                if font.getlength(words[word_counter]) > self.max_width:
-                    return TOO_LARGE_STRING
+                if self.font.getlength(words[word_counter]) > self.max_width:
+                    # make dynamic to font size in future
+                    return self.make_too_large_string_error()
                 current_line = words[word_counter]
                 if current_line[-1] == '\n':
                     text += current_line
@@ -73,7 +111,7 @@ class TextBox(Box):
                         word_counter += 1
                         continue
                     next_line = current_line + ' ' + words[word_counter]
-                    if font.getlength(next_line) > self.max_width:
+                    if self.font.getlength(next_line) > self.max_width:
                         break
                     current_line = next_line
                     if current_line[-1] == '\n':
@@ -81,82 +119,61 @@ class TextBox(Box):
                         break
                     word_counter += 1
                 text += current_line + ('' if current_line[-1] == '\n' else '\n')
-            text_box = self.drawer.draw.multiline_textbbox((0, 0), text, font=font, spacing=self.spacing)
+            text_box = CardDrawer.get_multiline_textbox(text, self.font, self.spacing)
             self.font_size -= self.size_step
             self.width = text_box[2] - text_box[0]
             self.height = text_box[3] - text_box[1]
             if self.height <= self.max_height:
                 break
         if self.height > self.max_height:
-            return TOO_LARGE_STRING
+            return self.make_too_large_string_error()
+        self.font_size = self.font.size
         return text
 
-    def update_content(self, content):
-        self.content = content
-        self.text = self.parse_content()
-
-
-class NameBox(Box):
-    def __init__(self, drawer, content='', font_path=DEFAULT_FONT, default_font_size=DEFAULT_NAME_FONT_SIZE,
-                 max_width=MAX_TEXT_WIDTH, max_height=MAX_NAME_HEIGHT):
-        super().__init__(drawer)
-        self.content = content
-        self.font_path = font_path
-        self.default_font_size = default_font_size
-        self.max_width = max_width
-        self.max_height = max_height
-        self.text = self.parse_content()
-
-    def parse_content(self):
-        font = ImageFont.truetype(font=self.font_path, size=self.default_font_size)
-        text_box = self.drawer.draw.textbbox((0, 0), self.content, font)
+    def make_too_large_string_error(self):
+        self.font = ImageFont.truetype(font=self.font_path, size=ERROR_FONT_SIZE, encoding='UTF-8')
+        text_box = CardDrawer.get_multiline_textbox(TOO_LARGE_STRING, self.font, self.spacing)
         self.width = text_box[2] - text_box[0]
         self.height = text_box[3] - text_box[1]
-        if self.width > self.max_width or self.height > self.max_height:
-            return TOO_LARGE_STRING
-        return self.content
+        return TOO_LARGE_STRING
 
     def update_content(self, content):
         self.content = content
         self.text = self.parse_content()
 
 
-class ArtistBox(NameBox):
-    def __init__(self, drawer, content='', font_path=DEFAULT_FONT, font_size=DEFAULT_ARTIST_FONT_SIZE,
-                 max_width=MAX_ARTIST_WIDTH, max_height=MAX_ARTIST_HEIGHT):
-        super().__init__(drawer, content, font_path, font_size, max_width, max_height)
-        self.text = ARTIST_STRING + self.text
-
-    def update_content(self, content):
-        self.content = content
-        self.text = ARTIST_STRING + self.parse_content()
-
-
-class PowerBox(Box):
-    def __init__(self, drawer, content=0, font_path=DEFAULT_FONT, font_size=DEFAULT_NAME_FONT_SIZE,
-                 max_width=MAX_TEXT_WIDTH):
-        super().__init__(drawer)
-        self.content = int(content)
-        self.font_path = font_path
-        self.font_size = font_size
-        self.max_width = max_width
-        self.power = self.parse_content()
+class NumberBox(TextBox):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, default_font_size=DEFAULT_POWER_FONT_SIZE, spacing=0,
+                         max_width=MAX_POWER_WIDTH, max_height=MAX_POWER_HEIGHT, **kwargs)
 
     def parse_content(self):
-        font = ImageFont.truetype(self.font_path, self.font_size)
-        if font.getlength(str(self.content)) > self.max_width or self.content < 0:
-            return INVALID_NUMBER
+        self.font = ImageFont.truetype(self.font_path, self.font_size, encoding='UTF-8')
+        if self.content == '':
+            self.width = 0
+            self.height = 0
+            return ''
+        for symbol in self.content:
+            if symbol not in '0123456789':
+                return self.make_invalid_number_error()
+        if self.font.getlength(self.content) > self.max_width or int(self.content) < 0:
+            return self.make_invalid_number_error()
         else:
-            return str(self.content)
+            text_box = CardDrawer.get_textbox(self.content, self.font)
+            self.width = text_box[2] - text_box[0]
+            self.height = text_box[3] - text_box[1]
+            return self.content
 
-    def update_content(self, content):
-        self.content = int(content)
-        self.power = self.parse_content()
+    def make_invalid_number_error(self):
+        text_box = CardDrawer.get_textbox(INVALID_NUMBER, self.font)
+        self.width = text_box[2] - text_box[0]
+        self.height = text_box[3] - text_box[1]
+        return INVALID_NUMBER
 
 
-class IconBox(Box):
-    def __init__(self, drawer, icon_name=None):
-        super().__init__(drawer)
+class IconBox(BitmapBox):
+    def __init__(self, pos_x=0, pos_y=0, icon_name=None):
+        super().__init__(pos_x, pos_y)
         self.icon_name = icon_name
         if icon_name is None:
             self.path = None
@@ -166,7 +183,3 @@ class IconBox(Box):
                 self.width = im.width
                 self.height = im.height
 
-
-class TableBox(Box):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
